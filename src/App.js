dea84aeaccd18e9ca3aca99468325220471ea25e
KECO-00 <ksang98@icloud.com>
2022-05-18 23:47:41 +0900

    Updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, Button, Container, ToggleButtonGroup, ToggleButton, Row, Form, Col, Image } from 'react-bootstrap';
+import { Link, Route, Switch } from 'react-router-dom';
+import Detail from './Detail.js';
+
 
 function App() {
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="logo.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            MOOD
+          </Navbar.Brand>
+          <Button variant="outline-success">로그인</Button>
+        </Container>
+      </Navbar>
+      <Route exact path="/">
+        <ToggleButtonGroup type="checkbox" defaultValue={[1]} className="select_type">
+          <ToggleButton id="hotel" variant="dark" value={1}>
+            호텔
+          </ToggleButton>
+          <ToggleButton id="motel" variant="dark" value={2}>
+            모텔
+          </ToggleButton>
+          <ToggleButton id="pension" variant="dark" value={3}>
+            펜션
+          </ToggleButton>
+          <ToggleButton id="guesthouse" variant="dark" value={4}>
+            게스트하우스
+          </ToggleButton>
+        </ToggleButtonGroup>
+        <br/>
+        <ToggleButtonGroup type="checkbox" defaultValue={[1]} className="select_facility">
+          <ToggleButton id="wifi" variant="dark" value={1}>
+            WiFi
+          </ToggleButton>
+          <ToggleButton id="breakfast" variant="dark" value={2}>
+            조식제공
+          </ToggleButton>
+          <ToggleButton id="parking" variant="dark" value={3}>
+            주차가능
+          </ToggleButton>
+        </ToggleButtonGroup>
+
+        <Row className="searchTitle">
+          <Col>
+            <Form.Control type="text" placeholder="검색" />
+          </Col>
+        </Row>
+
+        <Link to='/detail'>
+          <div className="post">
+            <hr/>
+            <Image className="picture" src='hotel.jpeg'  />
+            <h4>서울가든호텔</h4>
+            <p>서울시 마포구<br/>4.6/5.0</p>
+          </div>
+        </Link>
+        <Link to='/detail'>
+        <div className="post">
+          <hr/>
+          <Image className="picture" src='hotel2.jpeg' />
+          <h4>롯데시티호텔 명동</h4>
+          <p>서울시 서초구<br/>4.5/5.0</p>
+        </div>
+        </Link>
+        <Link to='/detail'>
+          <div className="post">
+            <hr/>
+            <Image className="picture" src='hotel.jpeg' />
+            <h4>로얄 서울 호텔</h4>
+            <p>서울시 중구<br/>4.6/5.0</p> 
+          </div>
+        </Link>
+      </Route>
+
+      <Route path="/detail">
+        <Detail/>
+      </Route>
+      
+
+
+
     </div>
   );
 }
---
+++ src/Detail.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
+
+function Detail(){
+
+    let history = useHistory();
+
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+                    <p/> 
+                    <img src='hotel.jpeg' width="100%"/>
+                </div>
+            
+                <div className="col-md-6 mt-4">
+                    <h4 className="pt-5">호텔명</h4>
+                    <p>호텔 위치</p>
+                    <p>호텔 별점</p>
+                    <button className="btn btn-danger">예약 페이지로 이동</button>
+                    <button className="btn btn-danger" onClick={ () => { history.goBack(); }}>뒤로 가기</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Detail;
