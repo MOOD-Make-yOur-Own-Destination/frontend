67f8044c381ddaee6584b2de23b90e3925742151
KECO-00 <ksang98@icloud.com>
2022-05-19 14:16:20 +0900

    New: File of user Profile

---
+++ src/Profile.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Profile({ user }) {
+    const {id, password, nickname } = user || {};
+    return (
+        <>
+        <h1>Profile</h1>
+        <dt>ID</dt>
+        <dd>{ id }</dd>
+        <dt>Password</dt>
+        <dd>{ password }</dd>
+        <dt>Nickname</dt>
+        <dd>{ nickname } </dd>
+        </>
+    );
+}
+
+export default Profile;
