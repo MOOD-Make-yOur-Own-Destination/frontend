c6e549519999111f42a95e5d024f50e4a7b1c424
KECO-00 <ksang98@icloud.com>
2022-05-28 14:43:51 +0900

    Updated: 버튼 형식 수정

--- src/Detail.js
+++ src/Detail.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router';
 import { Link } from 'react-router-dom';
+import {Button} from 'react-bootstrap';
 
 function Detail(){
 
@@ -19,9 +20,9 @@ function Detail(){
                     <p>호텔 위치</p>
                     <p>호텔 별점</p>
                     <Link to={{ pathname: "https://www.naver.com" }} target="_blank">
-                        <button className="btn btn-danger">예약 페이지로 이동</button>
+                        <Button className="btn btn-danger">예약 페이지로 이동</Button>
                     </Link>
-                    <button className="btn btn-danger" onClick={ () => { history.goBack(); }}>뒤로 가기</button>
+                    <Button className="btn btn-danger" onClick={ () => { history.goBack(); }}>뒤로 가기</Button>
                 </div>
             </div>
         </div>
