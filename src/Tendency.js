dde53469c12dbf51203f90ea2e7553320a92e537
KECO-00 <ksang98@icloud.com>
2022-06-08 20:44:48 +0900

    Modified: Class --> ClassName으로 수정

--- src/Tendency.js
+++ src/Tendency.js
@@ -17,59 +17,59 @@ function Tendency(){
             <div className="tendencylist">
                 <h4>{question[0]}</h4>
                 <div>
-                    <input id="q1-1" class ="btn-check" type="radio" name="number1" autocomplete="off" />
-                    <label class="btn btn-outline-dark" for="q1-1">1</label>
+                    <input id="q1-1" className ="btn-check" type="radio" name="number1" autocomplete="off" />
+                    <label className="btn btn-outline-dark" for="q1-1">1</label>
 
-                    <input id="q1-2" class ="btn-check" type="radio" name="number1" autocomplete="off"/>
-                    <label class="btn btn-outline-dark" for="q1-2">2</label>
+                    <input id="q1-2" className ="btn-check" type="radio" name="number1" autocomplete="off"/>
+                    <label className="btn btn-outline-dark" for="q1-2">2</label>
 
-                    <input id="q1-3" class ="btn-check" type="radio" name="number1" autocomplete="off"/>
-                    <label class="btn btn-outline-dark" for="q1-3">3</label>
+                    <input id="q1-3" className ="btn-check" type="radio" name="number1" autocomplete="off"/>
+                    <label className="btn btn-outline-dark" for="q1-3">3</label>
 
-                    <input id="q1-4" class ="btn-check" type="radio" name="number1" autocomplete="off"/>
-                    <label class="btn btn-outline-dark" for="q1-4">4</label>
+                    <input id="q1-4" className ="btn-check" type="radio" name="number1" autocomplete="off"/>
+                    <label className="btn btn-outline-dark" for="q1-4">4</label>
 
-                    <input id="q1-5" class ="btn-check" type="radio" name="number1" autocomplete="off"/>
-                    <label class="btn btn-outline-dark" for="q1-5">5</label>
+                    <input id="q1-5" className ="btn-check" type="radio" name="number1" autocomplete="off"/>
+                    <label className="btn btn-outline-dark" for="q1-5">5</label>
                 </div>
             </div> 
             
             <div className="tendencylist">
                 <h4>{question[1]}</h4>
                 <div>
-                <input id="q2-1" class="btn-check" type="radio" value="one" name="number2" autocomplete="off"/>
-                <label class="btn btn-outline-dark" for="q2-1">1</label>
+                <input id="q2-1" className="btn-check" type="radio" value="one" name="number2" autocomplete="off"/>
+                <label className="btn btn-outline-dark" for="q2-1">1</label>
 
-                <input id="q2-2" class="btn-check" type="radio" value="two" name="number2" autocomplete="off"/>
-                <label class="btn btn-outline-dark" for="q2-2">2</label>
+                <input id="q2-2" className="btn-check" type="radio" value="two" name="number2" autocomplete="off"/>
+                <label className="btn btn-outline-dark" for="q2-2">2</label>
 
-                <input id="q2-3" class="btn-check" type="radio" value="two" name="number2" autocomplete="off"/>
-                <label class="btn btn-outline-dark" for="q2-3">3</label>
+                <input id="q2-3" className="btn-check" type="radio" value="two" name="number2" autocomplete="off"/>
+                <label className="btn btn-outline-dark" for="q2-3">3</label>
 
-                <input id="q2-4" class="btn-check" type="radio" value="two" name="number2" autocomplete="off"/>
-                <label class="btn btn-outline-dark" for="q2-4">4</label>
+                <input id="q2-4" className="btn-check" type="radio" value="two" name="number2" autocomplete="off"/>
+                <label className="btn btn-outline-dark" for="q2-4">4</label>
 
-                <input id="q2-5" class="btn-check" type="radio" value="two" name="number2" autocomplete="off"/>
-                <label class="btn btn-outline-dark" for="q2-5">5</label>
+                <input id="q2-5" className="btn-check" type="radio" value="two" name="number2" autocomplete="off"/>
+                <label className="btn btn-outline-dark" for="q2-5">5</label>
                 </div>
             </div> 
             <div className="tendencylist">
                 <h4>{question[2]}</h4>
                 <div>
-                <input id="q3-1" class="btn-check" type="radio" value="one" name="number3" autocomplete="off"/>
-                <label class="btn btn-outline-dark" for="q3-1">1</label>
+                <input id="q3-1" className="btn-check" type="radio" value="one" name="number3" autocomplete="off"/>
+                <label className="btn btn-outline-dark" for="q3-1">1</label>
 
-                <input id="q3-2" class="btn-check" type="radio" value="two" name="number3" autocomplete="off"/>
+                <input id="q3-2" className="btn-check" type="radio" value="two" name="number3" autocomplete="off"/>
                 <label class="btn btn-outline-dark" for="q3-2">2</label>
 
-                <input id="q3-3" class="btn-check" type="radio" value="three" name="number3" autocomplete="off"/>
-                <label class="btn btn-outline-dark" for="q3-3">3</label>
+                <input id="q3-3" className="btn-check" type="radio" value="three" name="number3" autocomplete="off"/>
+                <label className="btn btn-outline-dark" for="q3-3">3</label>
 
-                <input id="q3-4" class="btn-check" type="radio" value="four" name="number3" autocomplete="off"/>
-                <label class="btn btn-outline-dark" for="q3-4">4</label>
+                <input id="q3-4" className="btn-check" type="radio" value="four" name="number3" autocomplete="off"/>
+                <label className="btn btn-outline-dark" for="q3-4">4</label>
 
-                <input id="q3-5" class="btn-check" type="radio" value="five" name="number3" autocomplete="off"/>
-                <label class="btn btn-outline-dark" for="q3-5">5</label>
+                <input id="q3-5" className="btn-check" type="radio" value="five" name="number3" autocomplete="off"/>
+                <label className="btn btn-outline-dark" for="q3-5">5</label>
                 </div>
             </div> 
             <br/>
